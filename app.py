2861c603cb9bed8b2f5ef1a4f07ffbca6c2dfee8
SupreethK1 <41055021+SupreethK1@users.noreply.github.com>
2021-11-27 02:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def calculator():
-    # Program make a simple calculator
-    # This function adds two numbers
-    def add(x, y):
-        return x + y
-
-    # This function subtracts two numbers
-    def subtract(x, y):
-        return x - y
-
-    # This function multiplies two numbers
-    def multiply(x, y):
-        return x * y
-
-    # This function divides two numbers
-    def divide(x, y):
-        return x / y
+    # Program make a simple calculator    
     print("Select operation.")
     print("1.Add")
     print("2.Subtract")
@@ -35,16 +20,16 @@ def calculator():
             num2 = float(input("Enter second number: "))
 
             if choice == '1':
-                print(num1, "+", num2, "=", add(num1, num2))
+                print(num1, "+", num2, "=", num1 + num2)
 
             elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1, num2))
+                print(num1, "-", num2, "=", num1 - num2)
 
             elif choice == '3':
-                print(num1, "*", num2, "=", multiply(num1, num2))
+                print(num1, "*", num2, "=", num1 * num2)
 
             elif choice == '4':
-                print(num1, "/", num2, "=", divide(num1, num2))
+                print(num1, "/", num2, "=", num1 / num2)
         
             # check if user wants another calculation
             # break the while loop if answer is no
